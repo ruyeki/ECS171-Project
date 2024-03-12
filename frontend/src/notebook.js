77fa1ec34ff98b5009d87009eca56624a890f34b
Ryan Uyeki <ryanuyeki@Ryans-MacBook-Air-4.local>
2024-03-12 01:27:56 -0700

    added react app

---
+++ frontend/src/notebook.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Notebook() {
+    return (
+        <iframe src="project_frontend.html" width="100%" height="600px"></iframe>
+    );
+}
+
+export default Notebook;
